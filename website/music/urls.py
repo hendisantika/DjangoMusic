968ddddc7225be71718ad36bd682772cc778b5e6
hendisantika <hendisantika@yahoo.co.id>
2019-02-05 18:47:39 +0700

    #8 - Set music url

---
+++ website/music/urls.py
@@ -0,0 +1,9 @@
+from django.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    url(r'^edit/(?P<pk>\d+)$', views.edit, name='edit'),
+    url(r'^delete/(?P<pk>\d+)$', views.delete, name='delete'),
+]
